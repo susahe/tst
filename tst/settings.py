7253b6e6c4f076b8b4bd7c9d82140cbb1bb4eac3
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-29 01:25:02 +0530

    lot of experiment with polls models and template

--- tst/settings.py
+++ tst/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'polls',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -52,10 +53,11 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'tst.urls'
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
